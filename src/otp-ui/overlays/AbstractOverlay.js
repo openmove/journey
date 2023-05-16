98f828fe042994d77b0f47cd564a32e198d02008
Ghoster981 <marcobonora.98@gmail.com>
2023-05-16 10:12:21 +0200

    minor fix

--- src/otp-ui/overlays/AbstractOverlay.js
+++ src/otp-ui/overlays/AbstractOverlay.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import {  MapLayer } from 'react-leaflet'
+import { getItem } from "../core-utils/storage";
 
 class AbstractOverlay extends MapLayer {
 
