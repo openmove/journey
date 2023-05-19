0d9624f07e82ae4d0a5b2d2f57333afa276c3d6f
Ghoster981 <marcobonora.98@gmail.com>
2023-05-19 10:52:53 +0200

    pt2 added default configuration for trip overlay

--- src/components/map/connected-trip-viewer-overlay.js
+++ src/components/map/connected-trip-viewer-overlay.js
@@ -1,11 +1,22 @@
+import { getRouteColor } from '../../otp-ui/itinerary-body/util'
 import TripViewerOverlay from '../../otp-ui/overlay-trip-viewer'
 import { connect } from 'react-redux'
 
 // connect to the redux store
 
 const mapStateToProps = (state, ownProps) => {
-  const viewedTrip = state.otp.ui.viewedTrip
+  const viewedTrip = state.otp.ui?.viewedTrip
+  // const routeColor = state.otp.transitIndex?.trips[viewedTrip?.tripId]?.route?.mode?.route?.color
+
+  // const color = getRouteColor(routeColor)
+  // TODO: overwrite default color with route specific one if present
+  const style = state?.otp?.config?.trip?.style
+ /*  if(color){
+    style.color = color;
+  }
+ */
   return {
+    style,
     tripData: viewedTrip
       ? state.otp.transitIndex.trips[viewedTrip.tripId]
       : null
