de1fa4de15adf757b4791d2750344c48c6b4b897
Ghoster981 <marcobonora.98@gmail.com>
2023-06-09 15:26:42 +0200

    added lang attribute to html and when language change to prevent google translate popup

--- src/components/app/menu-item/language-dropdown.js
+++ src/components/app/menu-item/language-dropdown.js
@@ -29,7 +29,7 @@ class LanguageDropDown extends Component{
           {langList.map((slang, index) => {
             return (
               <MenuItem
-                onClick={() => i18n.changeLanguage(slang.lang)}
+                onClick={() =>{ i18n.changeLanguage(slang.lang); document.documentElement.lang = i18n.language;}}
                 key={index}
               >
                 {slang.label}
