8d65f925aea2a8fbe78b4cd4bccdbdd2bc118331
Ghoster981 <marcobonora.98@gmail.com>
2024-10-21 14:08:51 +0200

    cleaned up geojson overlay since it's not "really" an overlay

--- src/otp-ui/overlays/geojson/index.js
+++ src/otp-ui/overlays/geojson/index.js
@@ -1,83 +1,54 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { LayerGroup, FeatureGroup, GeoJSON, MapLayer, Marker, Popup, withLeaflet, Polyline } from 'react-leaflet'
-import { divIcon } from 'leaflet'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import {
+  LayerGroup,
+  FeatureGroup,
+  GeoJSON,
+  MapLayer,
+  Marker,
+  Popup,
+  withLeaflet,
+  Polyline,
+} from "react-leaflet";
+import { divIcon } from "leaflet";
 import { withNamespaces } from "react-i18next";
 import { Button } from "react-bootstrap";
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import { getItem } from "../../core-utils/storage";
-import { setLocation } from '../../../actions/map';
-import { geojsonLocationsQuery } from '../../../actions/geojson';
+import { geojsonLocationsQuery } from "../../../actions/geojson";
 
 //import ReactDOMServer from "react-dom/server";
 
-
 class GeojsonOverlay extends MapLayer {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this._startRefreshing = this._startRefreshing.bind(this)
-    this._stopRefreshing = this._stopRefreshing.bind(this)
+    this._startRefreshing = this.update.bind(this);
+    this._stopRefreshing = this._stopRefreshing.bind(this);
   }
 
   static propTypes = {
     api: PropTypes.string,
     geojsonLocationsQuery: PropTypes.func,
-    setLocation: PropTypes.func
-  }
+    setLocation: PropTypes.func,
+  };
 
-  _startRefreshing() {
+  update() {
     // ititial station retrieval
-    this.props.geojsonLocationsQuery(this.props.api)
-    const {overlayGeojsonConf} = this.props;
-
-    // set up timer to refresh stations periodically
-    if (this._refreshTimer)  clearInterval(this._refreshTimer) // needed to not create multiple intervals
-    this._refreshTimer = setInterval(() => {
-      this.props.geojsonLocationsQuery(this.props.api)
-    }, Number(overlayGeojsonConf.pollingInterval || 30000)) // defaults to every 30 sec. TODO: make this configurable?*/
-  }
-
-  _stopRefreshing() {
-    if (this._refreshTimer) clearInterval(this._refreshTimer)
+    const { geojsonConfig } = this.props;
+    this.props.geojsonLocationsQuery(geojsonConfig?.api);
   }
 
   componentDidMount() {
-    this.props.registerOverlay(this)
-
-    if (this.props.visible) {
-      this._startRefreshing()
-    }
-    //this.props.leaflet.map.on('zoomend', this.updatePolylineWeigth);
+      this.update();
   }
 
   componentWillUnmount() {
-    this._stopRefreshing()
-    //this.props.leaflet.map.off('zoomend', this.updatePolylineWeigth);
   }
 
-  onOverlayAdded = (e) => {
-    this._startRefreshing();
-    const { map } = this.props.leaflet;
-    const {overlayGeojsonConf} = this.props;
-
-    if(overlayGeojsonConf.startCenter){
-      map.flyTo(overlayGeojsonConf.startCenter);
-    }
-  }
-
-  onOverlayRemoved = () => {
-    this._stopRefreshing()
-  }
 
   componentDidUpdate(prevProps) {
-    if (!prevProps.visible && this.props.visible) {
-      this._startRefreshing()
-    } else if (prevProps.visible && !this.props.visible) {
-      this._stopRefreshing()
-    }
   }
 
   createLeafletElement() { }
@@ -85,42 +56,43 @@ class GeojsonOverlay extends MapLayer {
   updateLeafletElement() { }
 
   render() {
-    const { geojson, overlayGeojsonConf } = this.props
+    const { geojson, geojsonConfig } = this.props;
 
-    const getStyle = feature => ({
-      weight: 4,
-      //opacity: !feature.properties.level ? 0.4 : 1,
-      color: overlayGeojsonConf.color || '#dd0000'
-    });
+    const getStyle = (feature) => {
+      return {
+        weight: feature.properties.name == "area" ? 0 : 2,
+        opacity: feature.properties.name == "area" ? 0.0001 : 1,
+        color:
+          feature.properties.name == "area"
+            ? geojsonConfig.bgColor || "#dd0000"
+            : geojsonConfig.color || "#dd0000",
+      };
+    };
 
-    if (!geojson ||
-      !geojson?.features ||
-      geojson?.features.length === 0) return <LayerGroup />
-
+    if (!geojson || !geojson?.features || geojson?.features.length === 0)
+      return <LayerGroup />;
 
     return (
       <LayerGroup>
-        <GeoJSON
-          data={geojson?.features}
-          style={getStyle}
-        />
+        <GeoJSON data={geojson?.features} style={getStyle} />
       </LayerGroup>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    overlayGeojsonConf: state.otp?.config?.map?.overlays?.filter(item => item.type === 'geojson')[0],
-    geojson: state.otp.overlay.geojson && state.otp.overlay.geojson.geojson
-  }
-}
+    overlayGeojsonConf: state.otp?.config?.map?.overlays?.filter(
+      (item) => item.type === "geojson"
+    )[0],
+    geojson: state.otp.overlay.geojson && state.otp.overlay.geojson.geojson,
+  };
+};
 
 const mapDispatchToProps = {
-  setLocation,
-  geojsonLocationsQuery
-}
+  geojsonLocationsQuery,
+};
 
-export default withNamespaces()(connect(mapStateToProps, mapDispatchToProps)(withLeaflet(GeojsonOverlay)))
-
-
+export default withNamespaces()(
+  connect(mapStateToProps, mapDispatchToProps)(withLeaflet(GeojsonOverlay))
+);
