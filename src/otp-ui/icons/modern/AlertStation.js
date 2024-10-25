bdcfeab33319c58ade55b876a740b6fe0850964a
Ghoster981 <marcobonora.98@gmail.com>
2024-10-25 09:35:55 +0200

    Alert icon

--- src/otp-ui/icons/modern/AlertStation.js
+++ src/otp-ui/icons/modern/AlertStation.js
@@ -3,10 +3,10 @@ import React from "react";
 
 const  AlertStation = ({ title,img, width = "100%", height="100%", ...props }) => (
   <img
-    alt={title ? <title>{title}</title> : "Hopr Icon"}
+    alt={title ? <title>{title}</title> : "Events Icon"}
     height={height}
     width={width}
-    src={'http://127.0.0.1:5500/events/'+ (img || 'images/0.png')}
+    src={ img || 'images/0.png'} //todo fallback
     {...props}
   />
 );
